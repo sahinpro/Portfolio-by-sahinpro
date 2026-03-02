9b41e96103041857f6cec6c85a3bb8a6f427ce70
sahinhub <sahinhub@gmail.com>
2026-03-02 07:49:14 +0600

    GASP changed to Framer Motion fixed

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HomePage } from "@/pages/HomePage";
 import { AboutPage } from "@/pages/AboutPage";
+import { ContactPage } from "@/pages/ContactPage";
+import { HomePage } from "@/pages/HomePage";
+import { NotFoundPage } from "@/pages/NotFoundPage";
 import { ProjectsPage } from "@/pages/ProjectsPage";
 import { ServicesPage } from "@/pages/ServicesPage";
-import { ContactPage } from "@/pages/ContactPage";
-import { NotFoundPage } from "@/pages/NotFoundPage";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "../tailwind.css";
 
-const root = document.getElementById("app") as HTMLElement;
+const root = document.getElementById("root") as HTMLElement;
 
 createRoot(root).render(
   <StrictMode>
